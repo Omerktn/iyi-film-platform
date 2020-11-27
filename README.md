dd4968de6479324a24618617c608f0b18a3be231
Omerktn <o.kurttekin@hotmail.com>
2020-11-27 14:51:25 +0300

    basic jsonfiy API added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# sistem-prog
+# İYİFİLM - Film/Dizi Tartışma Platformu
+
+## Başlarken yapılacaklar
+
+Yeni virutal enviroment oluşturun.
+Bu sizi normal bilgisayarınızdaki Python'dan ayrıştıracaktır.
+``` python3 -m venv venv ```
+
+Aktive ettiğiniz zaman artık bu virtualenv içindesiniz demektir. Dışarıdan izolesiniz, her zaman buradan devam edeceğiz bu proje için.
+Not: Herkesin kendi bilgisayarına özel.
+``` source venv/bin/activate ```
+
+Gerekli python kütüphanelerini kurmak için.
+``` pip3 install -r requirements.txt```
+
+Projeyi çalıştırabilirsiniz.
+``` python3 run.py ```
+
--- iyifilm/routes.py
+++ iyifilm/routes.py
@@ -1,6 +1,41 @@
 from iyifilm import app, bcrypt, db
 
+from flask import request, jsonify
+
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
 def home():
     return "Hello, this is IYI Film"
+
+films = [
+    {"id": 0,
+     "title": "Limonata"},
+    {"id": 1,
+     "title": "Ölümlü Dünya"},
+    {"id": 2,
+     "title": "Trainspotting"},
+]
+
+# Örneğin http://127.0.0.1:5000/films/all adresi bütün
+# filmleri döndürecek.
+@app.route('/films/all', methods=['GET'])
+def film_all():
+    return jsonify(films)
+
+# Bu API'a örneğin şu şekilde ulaşılacak:
+# http://127.0.0.1:5000/films/?id=2
+@app.route('/films/', methods=['GET'])
+def film_by_id():
+    if "id" in request.args:
+        id = int(request.args["id"])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    results = []
+
+    global films
+    for film in films:
+        if film["id"] == id:
+            results.append(film)
+
+    return jsonify(results)
